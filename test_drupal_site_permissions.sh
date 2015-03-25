ccc98161d0aae60f66aeb911df0b6af650e15ba8
James Reidy <jgr25@cornell.edu>
2015-03-25 13:24:50 -0400

    test scipt for local use

---
+++ test_drupal_site_permissions.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# test_drupal_site_permissions.sh
+
+pushd `dirname $0` > /dev/null
+SCRIPTPATH=`pwd`
+popd > /dev/null
+
+filename=$(basename $0)
+SCRIPT_ID="${filename%.*}"
+
+REMOTE_MACHINE=victoria01.library.cornell.edu
+REMOTE_SITE_NAME=$1
+REMOTE_PATH=/libweb/sites/${REMOTE_SITE_NAME}/htdocs
+REMOTE_FILES_PATH=/libweb/sites/${REMOTE_SITE_NAME}/drupal_files
+REMOTE_BACKUP_PATH=${REMOTE_FILES_PATH}/movers/$SCRIPT_ID
+REMOTE_USER=$USER
+
+LOCAL_MACHINE=victoria02.serverfarm.cornell.edu
+LOCAL_SITE_NAME=LAW
+LOCAL_PATH=/Applications/MAMP/htdocs/law
+LOCAL_FILES_PATH=/Applications/MAMP/htdocs/law_files
+LOCAL_BACKUP_PATH=${LOCAL_FILES_PATH}/movers/$SCRIPT_ID
+LOCAL_USER=$USER
+LOCAL_USER_GROUP=staff
+LOCAL_USER_PHP=admin
+
+#always include at least default
+SUBSITES=(default )
+
+#*********************************************
+#*********************************************
+#*** customize above here
+#*********************************************
+#*********************************************
+
+BASEDIR=$(dirname $0)
+source ${BASEDIR}/flow_functions.sh
+
+source ${SCRIPTPATH}/drupal_site_permissions.sh
