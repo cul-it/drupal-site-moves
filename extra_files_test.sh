1a27032118fba207e5a04a6fd3375f4c096605a3
James Reidy <jgr25@cornell.edu>
2015-03-27 08:52:34 -0400

    show hostname

--- extra_files_test.sh
+++ extra_files_test.sh
@@ -39,7 +39,7 @@ LIBRARIES="$LOCAL_PATH/site/all/libraries"
 
 tests=("$LOCAL_PATH" "$LOCAL_PRIVATE_FILES_PATH" "$LOCAL_SITE_MOVES_DIRECTORY" "$SITES_FILES" "$MODULES" "$THEMES" "$LIBRARIES")
 
-echo ""
+echo "Host: $HOSTNAME"
 
 for test in "${tests[@]}"
 do
