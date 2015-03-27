456c805cb0eb37cbf6b4f94b753ea7c6a7f506e1
James Reidy <jgr25@cornell.edu>
2015-03-27 11:57:54 -0400

    more dev

--- move_latest_scripts_to_puppet.sh
+++ move_latest_scripts_to_puppet.sh
@@ -8,12 +8,16 @@ popd > /dev/null
 filename=$(basename $0)
 SCRIPT_ID="${filename%.*}"
 
+
+STAMP=`date +'%Y%m%d_%H%M%S'`
+
+WORK_DIR="$HOME/${SCRIPT_ID}_${STAMP}"
 SVN_SCRIPTS=https://svn.library.cornell.edu/cul-drupal/scripts
-SVN_SCRIPT_DIR=scripts
+SVN_SCRIPTS_DIR="$WORK_DIR/scripts"
 GIT_MOVER_SCRIPTS=git@github.com:cul-it/drupal-site-moves.git
-GIT_MOVER_SCRIPT_DIR="drupal-site-moves"
+GIT_MOVER_SCRIPTS_DIR="$WORK_DIR/drupal-site-moves"
 PUPPET_SCRIPTS=https://svn.library.cornell.edu/puppet/modules/webserver/files/drupal
-PUPPET_SCRIPTS_DIR=drupal
+PUPPET_SCRIPTS_DIR="$WORK_DIR/drupal"
 
 BASEDIR=$(dirname $0)
 source ${BASEDIR}/flow_functions.sh
@@ -27,15 +31,16 @@ function usage() {
 
 [ "$1" = "--fire-bazooka" ] || usage
 
-STAMP=`date +'%Y%m%d_%H%M%S'`
+message "working in $WORK_DIR"
 
-WORK_DIR="~/{SCRIPT_ID}_${STAMP}"
-
-mkdir -p $WORK_DIR || error_exit "mkdir $WORK_DIR failed"
-cd $WORK_DIR || error_exit "cd to $WORK_DIR failed"
-svn co "$SVN_SCRIPTS" && [ -d "$SVN_SCRITPS_DIR" ] || error_exit "can't get $SVN_SCRIPTS"
-git clone "$GIT_MOVER_SCRIPTS" && [ -d "GIT_MOVER_SCRIPTS_DIR" ] || error_exit "can't get $GIT_MOVER_SCRIPTS"
-svn co "$PUPPET_SCRIPTS" && [ -d "$PUPPET_SCRIPTS_DIR" ] || error_exit "can't get $PUPPET_SCRIPTS"
+mkdir -p "$WORK_DIR" || error_exit "mkdir $WORK_DIR failed"
+cd "$WORK_DIR" || error_exit "cd to $WORK_DIR failed"
+echo "Building $SVN_SCRIPTS_DIR"
+svn co --quiet "$SVN_SCRIPTS" && [ -d "$SVN_SCRIPTS_DIR" ] || error_exit "can't get $SVN_SCRIPTS"
+echo "Building $GIT_MOVER_SCRIPTS_DIR"
+git clone -q "$GIT_MOVER_SCRIPTS" && [ -d "$GIT_MOVER_SCRIPTS_DIR" ] || error_exit "can't get $GIT_MOVER_SCRIPTS"
+echo "Building $PUPPET_SCRIPTS_DIR"
+svn co --quiet "$PUPPET_SCRIPTS" && [ -d "$PUPPET_SCRIPTS_DIR" ] || error_exit "can't get $PUPPET_SCRIPTS"
 
 svns=(dr-make.sh dr-move-db.sh dr-move-files.sh move-goldsen-to-production.sh move-goldsen-to-test.sh update_test_from_victoria03.sh update_victoria03_from_test.sh)
 svns_remove=(set_drupal_permissions.sh update_production_from_test.sh update_test_from_production.sh)
@@ -43,22 +48,34 @@ svns_remove=(set_drupal_permissions.sh update_production_from_test.sh update_tes
 for file in "${svns[@]}"
 do
   foo="$SVN_SCRIPTS_DIR/$file"
-  [ -f $foo ] && mv "$path" "$PUPPET_SCRIPTS_DIR/" || error_exit "can't move $foo"
+  target="$PUPPET_SCRIPTS_DIR/$file"
+  echo $foo
+  [ -f "$target" ] || NEWFILES+=("$foo")
+  [ -f $foo ] && mv "$foo" "$PUPPET_SCRIPTS_DIR/" || error_exit "can't move $foo"
 done
 
 for file in "${svns_remove[@]}"
 do
   foo="$PUPPET_SCRIPTS_DIR/$file"
+  target="$PUPPET_SCRIPTS_DIR/$file"
+  echo $foo
   if [ -f $foo ] ;then
-    svn delete "$foo" "$PUPPET_SCRIPTS_DIR/" || error_exit "can't remove $foo"
+    rm "$foo" || error_exit "can't remove $foo"
   fi
 done
 
 gits=(customize_robots_txt.sh drupal_site_permissions.sh find_who_runs_php.php flow_functions.sh local_drupal_db_restore.sh pull_site_from_production.sh pull_site_from_test.sh remote_drupal_db_backup.sh remote_drupal_pull_files.sh set_permissions.sh)
 for file in "${gits[@]}"
 do
-  foo="GIT_MOVER_SCRIPT_DIR/$file"
-  [ -f $foo ] && mv "$path" "$PUPPET_SCRIPTS_DIR/" || error_exit "can't move $foo"
+  foo="$GIT_MOVER_SCRIPTS_DIR/$file"
+  target="$PUPPET_SCRIPTS_DIR/$file"
+  echo $foo
+  [ -f "$target" ] || NEWFILES+=("$foo")
+  [ -f $foo ] && mv "$foo" "$PUPPET_SCRIPTS_DIR/" || error_exit "can't move $foo"
 done
 
-error_exit "check $WORK_DIR"
+message "committing the changes to svn for puppet"
+cd "$PUPPET_SCRIPTS_DIR" || error_exit "cd to $PUPPET_SCRIPTS_DIR failed"
+svn commit -m 'Latest versions of drupal site move and maker scripts from "$SCRIPT_ID"' || error_exit "commit failed"
+
+message "Done. Check $WORK_DIR"
