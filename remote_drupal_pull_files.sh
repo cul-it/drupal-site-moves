ba60ab20a0400e8a859f1f1d15f7814a66a47021
James Reidy <jreidy@cornell.edu>
2015-04-02 14:07:45 -0400

    remove {}

--- remote_drupal_pull_files.sh
+++ remote_drupal_pull_files.sh
@@ -25,8 +25,8 @@ message "remote_drupal_pull_files preflight"
 [ -d "$LOCAL_PRIVATE_FILES_PATH" ] || error_exit "no directory for $LOCAL_PRIVATE_FILES_PATH"
 
 # set up local backup directory
-mkdir -p -v "${LOCAL_SITE_MOVES_BACKUP_PATH}"
-[ -d "${LOCAL_SITE_MOVES_BACKUP_PATH}" ] || error_exit 'make directory $LOCAL_SITE_MOVES_BACKUP_PATH failed'
+mkdir -p -v "$LOCAL_SITE_MOVES_BACKUP_PATH"
+[ -d "$LOCAL_SITE_MOVES_BACKUP_PATH" ] || error_exit "make directory $LOCAL_SITE_MOVES_BACKUP_PATH failed"
 
 echo "...CHECK"
 
