0511a3060f18a7829dc631e756ceff976b1bb6c1
James Reidy <jreidy@cornell.edu>
2015-04-02 16:16:45 -0400

    mode or mkdir

--- remote_drupal_db_backup.sh
+++ remote_drupal_db_backup.sh
@@ -30,7 +30,7 @@ function rcmd () {
 }
 
 # set up remote backup directory
-rcmd "mkdir -pv ${REMOTE_SITE_MOVES_BACKUP_PATH}"
+rcmd "mkdir -pv -m ug=rwX,o=rX ${REMOTE_SITE_MOVES_BACKUP_PATH}"
 rcmd "[ -d ${REMOTE_SITE_MOVES_BACKUP_PATH} ] || echo 'make directory failed' "
 rcmd "echo ${STAMP} > ${REMOTE_TIMESTAMP_FILE} || echo 'write timestamp failed' && exit 1 "
 echo "...CHECK"
