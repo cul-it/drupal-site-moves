f52b9b8a2e0d24d3f482910227b191abad95f4d6
James Reidy <jreidy@cornell.edu>
2017-10-03 11:13:46 -0400

    add archive_cleanup.sh

---
+++ archive_cleanup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# archive_cleanup.sh - remove archived sites
+
+SCRIPT="site_archive"
+BASEDIR=$(dirname $0)
+source ${BASEDIR}/flow_functions.sh
+
+[ -z "$flow_functions" ] && echo "$0 requires flow_functions.sh" && exit 1
+
+TEMP_DIR="/tmp/$USER/$SCRIPT"
+rm -r -i "$TEMP_DIR/*"
