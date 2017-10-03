a615517c454ebf5955e72f06842121fce3821310
James Reidy <jreidy@cornell.edu>
2017-10-03 14:17:14 -0400

    Remove tar --gzip option

--- archive_wwwtest.sh
+++ archive_wwwtest.sh
@@ -20,13 +20,13 @@ sudo mkdir -p "$TEMP_DIR" || error_exit "Can not create $TEMP_DIR"
 sudo chown -R "$USER" "/tmp/$USER" || error_exit "Can not set permissions /tmp/$USER."
 
 STAMP=`date +'%Y%m%d_%H%M%S'`
-TEMP_FILE="$TEMP_DIR/$SITENAME-$STAMP-archive.tar.gz"
+TEMP_FILE="$TEMP_DIR/$SITENAME-$STAMP-archive.tar"
 
 sudo mkdir -p "$TEMP_DIR" || error_exit "Could not create $TEMP_DIR"
 sudo chown "$USER" "$TEMP_DIR" || error_exit "Could not make $TEMP_DIR writable"
 
 message "Creating archive..."
 
-drush archive-dump --destination="$TEMP_FILE" --tar-options="--exclude=.git --gzip" || "Could not create archive $TEMP_FILE"
+drush archive-dump --destination="$TEMP_FILE" --tar-options="--exclude=.git" || "Could not create archive $TEMP_FILE"
 
 message "Archive:" "$TEMP_FILE" "$USER@$HOSTNAME:$TEMP_FILE"
